14f3e487cbf567419ea2b8e3d5999df02013fa4e
QuangHuy014 <huynq201104@gmail.com>
2024-03-26 12:24:40 +0700

    b

--- PJWebBanThuoc/src/main/java/vn/webbanthuoc/controller/web/homeController.java
+++ PJWebBanThuoc/src/main/java/vn/webbanthuoc/controller/web/homeController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class homeController
  */
-@WebServlet("/homeController")
+@WebServlet({"/homeController"})
 public class homeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
