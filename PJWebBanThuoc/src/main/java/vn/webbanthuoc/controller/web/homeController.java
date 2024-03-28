a2389e503ef3e3b037e1c4a5071eb20ebaa7236e
DESKTOP-NVVL5LH\dnhan <dnhan.a7.c3tqcap@gmail.com>
2024-03-28 19:28:03 +0700

    no message

--- PJWebBanThuoc/src/main/java/vn/webbanthuoc/controller/web/homeController.java
+++ PJWebBanThuoc/src/main/java/vn/webbanthuoc/controller/web/homeController.java
@@ -1,65 +1,4 @@
-<<<<<<< HEAD
-package vn.webbanthuoc.controller.web;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class homeController
- */
-@WebServlet({"/homeController","/product-Detail","/cart"})
-public class homeController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public homeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/** 
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		RequestDispatcher rd = request.getRequestDispatcher("/views/web/trangchu.jsp");
-//		rd.forward(request, response);
-		String action=request.getServletPath();
-		try {
-			switch (action) {
-			case "/homeController": {
-				RequestDispatcher rd = request.getRequestDispatcher("/views/web/trangchu.jsp");
-				rd.forward(request, response);
-				break;
-			}
-			case "/product-Detail": {
-				RequestDispatcher rd = request.getRequestDispatcher("/views/web/productDetail.jsp");
-				rd.forward(request, response);
-				break;
-			}
-			case "/cart": {
-				RequestDispatcher rd = request.getRequestDispatcher("/views/web/cart.jsp");
-				rd.forward(request, response);
-				break;
-			}
-			default:
-				throw new IllegalArgumentException("Unexpected value: " + action);
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
-
-}
-
-=======
 package vn.webbanthuoc.controller.web;
 
 import java.io.IOException;
@@ -126,4 +65,4 @@ public class homeController extends HttpServlet {
 	}
 
 }
->>>>>>> 8b4269e (no message)
+
