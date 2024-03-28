6c9fa608aa01fb16d1768937a41c39283f55ac34
DESKTOP-NVVL5LH\dnhan <dnhan.a7.c3tqcap@gmail.com>
2024-03-28 11:43:47 +0700

    no message

--- PJWebBanThuoc/src/main/java/vn/webbanthuoc/controller/web/homeController.java
+++ PJWebBanThuoc/src/main/java/vn/webbanthuoc/controller/web/homeController.java
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-package vn.webbanthuoc.controller.web;
-
-import java.io.IOException;
-
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class homeController
- */
-@WebServlet("/homeController")
-public class homeController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public homeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/views/web/trangchu.jsp");
-		rd.forward(request, response);
-	}
-//		String action = request.getServletPath();
-//		try {
-//			switch (action) {
-//			case "/homeController": {
-//				RequestDispatcher rd = request.getRequestDispatcher("/views/web/trangchu.jsp");
-//			rd.forward(request, response);
-//				break;
-//			}case "/checkoutController":{
-//				RequestDispatcher rd = request.getRequestDispatcher("/views/web/trangcheckout.jsp");
-//				rd.forward(request, response);
-//				break;
-//				
-//			}
-//			default:
-//				throw new IllegalArgumentException("Unexpected value: " + action);
-//			}
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//		}
-//	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
-		
-		
-		
-	}
-
-}
-=======
 package vn.webbanthuoc.controller.web;
 
 import java.io.IOException;
@@ -124,12 +56,5 @@ public class homeController extends HttpServlet {
 		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
 }
->>>>>>> main
+
