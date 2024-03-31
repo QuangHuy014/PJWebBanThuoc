3f81ee7ccfa9d656465818652dc7510a5a93e129
QuangHuy014 <huynq201104@gmail.com>
2024-03-31 16:01:40 +0700

    no message

--- PJWebBanThuoc/src/main/java/vn/webbanthuoc/entity/hoaDon.java
+++ PJWebBanThuoc/src/main/java/vn/webbanthuoc/entity/hoaDon.java
@@ -1,5 +1,26 @@
 package vn.webbanthuoc.entity;
 
-public class hoaDon {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.List;
 
+@Entity
+@Table(name = "HoaDon")
+public class hoaDon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idHoaDon;
+    private LocalDate ngayTao;
+    private double tongTien;
+    
+    // Mối quan hệ với HoaDonChiTiet
+    @OneToMany(mappedBy = "hoaDon")
+    private List<hoaDonChiTiet> chiTietHoaDonList;
+    
+    // Constructors, getters and setters
 }
