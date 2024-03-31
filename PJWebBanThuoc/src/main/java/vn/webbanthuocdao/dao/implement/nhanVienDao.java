c5604d8502e2f6e77f7a96ab5397807483a6f296
Cong Nghia IT <nghia2801dt@gmail.com>
2024-03-31 21:55:12 +0700

    no message

--- PJWebBanThuoc/src/main/java/vn/webbanthuocdao/dao/implement/nhanVienDao.java
+++ PJWebBanThuoc/src/main/java/vn/webbanthuocdao/dao/implement/nhanVienDao.java
@@ -1,8 +1,46 @@
 package vn.webbanthuocdao.dao.implement;
 
+import java.util.List;
+
 import vn.webbanthuoc.dao.IDao;
 import vn.webbanthuoc.entity.nhanVien;
 
 public class nhanVienDao implements IDao<nhanVien>{
+
+	@Override
+	public void create(nhanVien entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(nhanVien entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public nhanVien delete(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public nhanVien findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<nhanVien> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
